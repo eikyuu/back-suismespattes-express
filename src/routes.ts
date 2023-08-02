5bd7a9c5ea8657fce3a4773a3f242c4faa32b396
eikyuu <v.duguet.dev@gmail.com>
2023-08-02 23:34:32 +0200

    remove user

--- src/routes.ts
+++ src/routes.ts
@@ -1,27 +1,6 @@
-import { UserController } from "./controller/UserController"
 import { WalkController } from './controller/WalkController'
 
-export const Routes = [{
-    method: "get",
-    route: "/users",
-    controller: UserController,
-    action: "all"
-}, {
-    method: "get",
-    route: "/users/:id",
-    controller: UserController,
-    action: "one"
-}, {
-    method: "post",
-    route: "/users",
-    controller: UserController,
-    action: "save"
-}, {
-    method: "delete",
-    route: "/users/:id",
-    controller: UserController,
-    action: "remove"
-},
+export const Routes = [
 // Walks
 {
     method: "get",
