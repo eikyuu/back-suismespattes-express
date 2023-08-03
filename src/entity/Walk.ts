341a5dd020a39a2e8e08144bf55c0ce454f4d2ae
vduguet <vduguet@apside.fr>
2023-08-03 09:20:36 +0200

    type

--- src/entity/Walk.ts
+++ src/entity/Walk.ts
@@ -4,49 +4,49 @@ import { WalkImage } from './WalkImage'
 @Entity()
 export class Walk {
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn("uuid" , { name : "id" })
     id: number
 
-    @Column({ name : "name" })
+    @Column("string", { name : "name", unique: true, length: 255, nullable: false, default: null })
     name: string
 
-    @Column()
+    @Column("string", { name : "slug", unique: true, length: 255, nullable: false, default: null })
     slug: string
 
-    @Column()
+    @Column("text", { name : "description", nullable: false, default: null })
     description: string
 
-    @Column()
+    @Column("string", { name : "city", length: 255, nullable: false, default: null })
     city: string
 
-    @Column({ name : "postal_code" })
+    @Column("string", { name : "postal_code", length: 5, nullable: false, default: null })
     postalCode: string
 
-    @Column()
+    @Column("string", { name : "street", length: 255, nullable: false, default: null })
     street: string
 
-    @Column()
+    @Column("string", { name : "country", length: 255, nullable: false, default: null })
     country: string
 
-    @Column({ name : "latitude", type: "float", nullable: true})
+    @Column("float", { name : "latitude", nullable: true })
     latitude: number
 
-    @Column({ name : "longitude", type: "float", nullable: true})
+    @Column("float", { name : "longitude", nullable: true })
     longitude: number
 
-    @Column({ name : "obligatory_leash" })
+    @Column("boolean", { name : "obligatory_leash", default: false })
     obligatoryLeash: boolean
 
-    @Column({ name : "water_point" })
+    @Column("boolean", { name : "water_point", default: false })
     waterPoint: boolean
 
-    @Column({ name : "processionary_caterpillar_alert" })
+    @Column("boolean", { name : "processionary_caterpillar_alert", default: false })
     processionaryCaterpillarAlert: boolean
 
-    @Column({ name : "cyanobacteria_alert"})
+    @Column("boolean", { name : "cyanobacteria_alert", default: false })
     cyanobacteriaAlert: boolean
 
-    @Column()
+    @Column("int", { name : "note", nullable: true })
     note: number
     
     @CreateDateColumn({ name : "created_at"})
