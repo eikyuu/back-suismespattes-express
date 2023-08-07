74c3b18c1a44ee4f40a3f969df650763848f993f
eikyuu <v.duguet.dev@gmail.com>
2023-08-07 23:11:48 +0200

    slug

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -1,5 +1,5 @@
 export const formatSlug = (slug) => {
-    slug = slug.normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/[^a-zA-Z0-9-]/g, "");
-    slug = slug.replace(/-{2,}/g, "-").replace(/^-|-$/g, "");
+    slug = slug.replace(/[^a-zA-Z0-9 ]/g, "");
+    slug = slug.replace(/\s+/g, "-");
     return slug.toLowerCase();
 }
